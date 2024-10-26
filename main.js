7aff0215a098836c43dba42c9faef5652c6fa983
Valentin1902 <162795289+Valentin1902@users.noreply.github.com>
2024-10-26 16:45:46 +0300

    =)

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const userContainer = document.getElementById("user-container");
+const userContainer = document.getElementById("user-container");
 
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((users) => {
-      users.forEach((user) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const userName = document.createElement("h3");
-        userName.textContent = user.name;
-        const userEmail = document.createElement("p");
-        userEmail.textContent = `Email: ${user.email}`;
-        const userAddress = document.createElement("p");
-        userAddress.textContent = `Місто: ${user.address.city}`;
-        card.appendChild(userName);
-        card.appendChild(userEmail);
-        card.appendChild(userAddress);
-        userContainer.appendChild(card);
-      });
-    })
-    .catch((error) => console.error("Помилка:", error));
-});
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((users) => {
+    users.forEach((user) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const userName = document.createElement("h3");
+      userName.textContent = user.name;
+      const userEmail = document.createElement("p");
+      userEmail.textContent = `Email: ${user.email}`;
+      const userAddress = document.createElement("p");
+      userAddress.textContent = `Місто: ${user.address.city}`;
+      card.appendChild(userName);
+      card.appendChild(userEmail);
+      card.appendChild(userAddress);
+      userContainer.appendChild(card);
+    });
+  })
+  .catch((error) => console.error("Помилка:", error));
